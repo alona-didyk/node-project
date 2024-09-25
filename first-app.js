30d2bfc3f97f3132a7b77b9884cdc3e2f3365697
Alona Didyk <alionadiduk31@gmail.com>
2024-09-25 12:04:24 +0300

    new commit

--- first-app.js
+++ first-app.js
@@ -65,24 +65,24 @@
 
 // app.listen(3000);
 
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // static serves static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+	res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
